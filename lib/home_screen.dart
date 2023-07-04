2847bd1c7cd36d3e865a75eb25d7de268b5ca0be
HIMA CHARAN GANGULA <himacharan128@gmail.com>
2023-07-04 19:47:03 +0530

    final release

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,20 +13,44 @@ class _HomeScreenState extends State<HomeScreen> {
     Book(
       title: 'Dune',
       author: 'Frank Herbert',
-      description: 'Dune is a science fiction novel by Frank Herbert.',
+      description: 'Dune is set in the distant future amidst a feudal '
+          'interstellar society in which various noble houses control '
+          'planetary fiefs. It tells the story of young Paul Atreides,'
+          ' whose family accepts the stewardship of the planet Arrakis. '
+          'While the planet is an inhospitable and sparsely populated '
+          'desert wasteland, it is the only source of melange, or "spice",'
+          ' a drug that extends life and enhances mental abilities. '
+          'Melange is also necessary for space navigation, which requires '
+          'a kind of multidimensional awareness and foresight that only '
+          'the drug provides. As melange can only be produced on Arrakis, '
+          'control of the planet is a coveted and dangerous undertaking. '
+          'The story explores the multilayered interactions of politics, '
+          'religion, ecology, technology, and human emotion, as the factions '
+          'of the empire confront each other in a struggle for the control of '
+          'Arrakis and its spice. ',
       imageUrl: 'assets/images/dune.png',
     ),
     Book(
       title: 'Ender\'s Game',
       author: 'Orson Scott Card',
       description:
-      'Ender\'s Game is a science fiction novel by Orson Scott Card.',
+      'Andrew "Ender" Wiggin thinks he is playing computer simulated war games; he is, '
+          'in fact, engaged in something far more desperate. The result of genetic experimentation,'
+          ' Ender may be the military genius Earth desperately needs in a war against an alien '
+          'enemy seeking to destroy all human life. The only way to find out is to throw Ender into '
+          'ever harsher training, to chip away and find the diamond inside, or destroy him utterly. Ender '
+          'Wiggin is six years old when it begins. He will grow up fast.',
       imageUrl: 'assets/images/ender.jpg',
     ),
     Book(
       title: 'Foundation',
       author: 'Isaac Asimov',
-      description: 'Foundation is a science fiction novel by Isaac Asimov.',
+      description: 'For twelve thousand years the Galactic Empire has ruled supreme. Now it is dying.'
+          ' But only Hari Seldon, creator of the revolutionary science of psychohistory, can see into'
+          ' the future--to a dark age of ignorance, barbarism, and warfare that will last thirty thousand '
+          'years. To preserve knowledge and save humankind, Seldon gathers the best minds in the Empire--both '
+          'scientists and scholars--and brings them to a bleak planet at the edge of the galaxy to serve as a '
+          'beacon of hope for future generations. He calls his sanctuary the Foundation.',
       imageUrl: 'assets/images/foundation.jpg',
     ),
     Book(
@@ -83,20 +107,44 @@ class _HomeScreenState extends State<HomeScreen> {
     Book(
       title: 'Dune',
       author: 'Frank Herbert',
-      description: 'Dune is a science fiction novel by Frank Herbert.',
+      description: 'Dune is set in the distant future amidst a feudal '
+          'interstellar society in which various noble houses control '
+          'planetary fiefs. It tells the story of young Paul Atreides,'
+          ' whose family accepts the stewardship of the planet Arrakis. '
+          'While the planet is an inhospitable and sparsely populated '
+          'desert wasteland, it is the only source of melange, or "spice",'
+          ' a drug that extends life and enhances mental abilities. '
+          'Melange is also necessary for space navigation, which requires '
+          'a kind of multidimensional awareness and foresight that only '
+          'the drug provides. As melange can only be produced on Arrakis, '
+          'control of the planet is a coveted and dangerous undertaking. '
+          'The story explores the multilayered interactions of politics, '
+          'religion, ecology, technology, and human emotion, as the factions '
+          'of the empire confront each other in a struggle for the control of '
+          'Arrakis and its spice. ',
       imageUrl: 'assets/images/dune.png',
     ),
     Book(
       title: 'Ender\'s Game',
       author: 'Orson Scott Card',
       description:
-      'Ender\'s Game is a science fiction novel by Orson Scott Card.',
+      'Andrew "Ender" Wiggin thinks he is playing computer simulated war games; he is, '
+          'in fact, engaged in something far more desperate. The result of genetic experimentation,'
+          ' Ender may be the military genius Earth desperately needs in a war against an alien '
+          'enemy seeking to destroy all human life. The only way to find out is to throw Ender into '
+          'ever harsher training, to chip away and find the diamond inside, or destroy him utterly. Ender '
+          'Wiggin is six years old when it begins. He will grow up fast.',
       imageUrl: 'assets/images/ender.jpg',
     ),
     Book(
       title: 'Foundation',
       author: 'Isaac Asimov',
-      description: 'Foundation is a science fiction novel by Isaac Asimov.',
+      description: 'For twelve thousand years the Galactic Empire has ruled supreme. Now it is dying.'
+          ' But only Hari Seldon, creator of the revolutionary science of psychohistory, can see into'
+          ' the future--to a dark age of ignorance, barbarism, and warfare that will last thirty thousand '
+          'years. To preserve knowledge and save humankind, Seldon gathers the best minds in the Empire--both '
+          'scientists and scholars--and brings them to a bleak planet at the edge of the galaxy to serve as a '
+          'beacon of hope for future generations. He calls his sanctuary the Foundation.',
       imageUrl: 'assets/images/foundation.jpg',
     ),
     Book(
